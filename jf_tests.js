d2531f96ecd92a38ff07ebcd42578d8260fe35a5
jason <jason>
2015-11-09 13:17:38 -0500

    Added basic Iron Router from this page: http://robertdickert.com/blog/2014/05/08/iron-router-first-steps/

--- jf_tests.js
+++ jf_tests.js
@@ -1,23 +1,43 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+  
 }
+
+Articles = new Meteor.Collection('articles');
+
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+
+    if (! Articles.findOne()){
+      var articles = [
+        {title: 'Article 1', body: 'This is article 1'},
+        {title: 'Article 2', body: 'This is article 2'},
+        {title: 'Article 3', body: 'This is article 3'}
+      ];
+      articles.forEach(function (article) {
+        Articles.insert(article);
+      })
+    }
+
+
   });
 }
+
+
+Router.map(function () {
+  this.route('about');  // By default, path = '/about', template = 'about'
+  this.route('home', {
+    path: '/',  //overrides the default '/home'
+  });
+  this.route('articles', {
+    data: function () {return Articles.find()}  //set template data context
+  });
+  this.route('article', {
+    path: '/article/:_id',
+    data: function () {return Articles.findOne({_id: this.params._id})},
+    template: 'fullArticle'
+  });
+});
+
+
