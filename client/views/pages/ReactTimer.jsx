628753a4578cd68336a4f0750ba4043be29ed0a1
jason <jason>
2015-11-19 15:46:29 -0500

    Added react for meteor and created a simple react "control" that is rendered when the about template loads.

---
+++ client/views/pages/ReactTimer.jsx
@@ -0,0 +1,33 @@
+   	var SetIntervalMixin = {
+	  	componentWillMount: function() {
+	    	this.intervals = [];
+	  	},
+	  	setInterval: function() {
+	    	this.intervals.push(setInterval.apply(null, arguments));
+	  	},
+	  	componentWillUnmount: function() {
+	    	this.intervals.map(clearInterval);
+	  	}
+	};
+
+	TickTock = React.createClass({
+		  	mixins: [SetIntervalMixin], // Use the mixin
+		  	getInitialState: function() {
+		    	return {seconds: 0};
+		  	},
+		  	componentDidMount: function() {
+		    	this.setInterval(this.tick, 1000); // Call a method on the mixin
+		  	},
+		  	tick: function() {
+		    	this.setState({seconds: this.state.seconds + 1});
+		  	},
+		  	render: function() {
+		    	return (
+		      	<p>
+		        React {this.props.name} has been running for {this.state.seconds} seconds.
+		      </p>
+		    );
+		}
+	});
+
+	//React.render(<TickTock />, document.getElementById("container"));
--- client/views/pages/about.js
+++ client/views/pages/about.js
@@ -1,3 +1,28 @@
+Template.about.rendered = function() {
+    if(!this._rendered) {
+      this._rendered = true;
+      console.log('Template onLoad');
+
+      // JSX Style
+      //ReactDOM.render(<TickTock />, document.getElementById("container"));
+      // JS Style: https://jsfiddle.net/reactjs/5vjqabv3/
+		ReactDOM.render(
+       		React.createElement(TickTock, {name: "C1"}),
+       		document.getElementById("container1")
+       	);
+
+       	ReactDOM.render(
+       		React.createElement(TickTock, {name: "C2"}),
+       		document.getElementById("container2")
+       	);
+
+       	ReactDOM.render(
+       		React.createElement(TickTock, {name: "C3"}),
+       		document.getElementById("container3")
+       	);
+    }
+}
+
   Template.about.events({
     'click .btnOnAmout': function () {  
       console.log("test");
